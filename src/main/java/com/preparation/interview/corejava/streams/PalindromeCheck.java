cba7d7459751eb78b3af19c0db235248010c98e8
Sangeeth S Kumar <sangeethskumar1@gmail.com>
2024-12-05 00:18:51 +0530

    Updates

---
+++ src/main/java/com/preparation/interview/corejava/streams/PalindromeCheck.java
@@ -0,0 +1,11 @@
+package com.preparation.interview.corejava.streams;
+
+/**
+ * Check if a given String is Palindrome
+ */
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+        String inputString = "malayalam";
+    }
+}
