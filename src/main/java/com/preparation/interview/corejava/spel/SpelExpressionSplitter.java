bf0d3658d986fe256842e4d37bef640655e25ab8
Sangeeth S Kumar <sangeethskumar1@gmail.com>
2025-02-24 12:01:36 +0530

    Updates

--- src/main/java/com/preparation/interview/corejava/spel/SpelExpressionSplitter.java
+++ src/main/java/com/preparation/interview/corejava/spel/SpelExpressionSplitter.java
@@ -1,17 +1,14 @@
 package com.preparation.interview.corejava.spel;
 
 import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 public class SpelExpressionSplitter {
     public static void main(String[] args) {
-        final String expression = "80-120";
+        final String expressionToParse = "80-120";
         final String delimiter = "-";
-        ExpressionParser parser = new SpelExpressionParser();
-        final String expressionToParse = String.format("'%s'.split('%s')", expression, delimiter);
-        Expression exp = parser.parseExpression(expressionToParse);
-        String[] result = (String[]) exp.getValue();
+        Expression expression = new SpelExpressionParser().parseExpression(String.format("'%s'.split('%s')", expressionToParse, delimiter));
+        String[] result = (String[]) expression.getValue();
         for (String str : result) {
             System.out.println(str);
         }
